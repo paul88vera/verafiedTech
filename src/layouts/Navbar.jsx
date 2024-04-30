730ce721913dc2e819d03d2dedfa6a8fa98808df
paul88vera <paul88vera@gmail.com>
2024-04-30 00:06:50 -0500

    temp landing

---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/Logo.png";
+
+export const Navbar = () => {
+  const linkStyle = "text-white text-[1.2rem]";
+
+  return (
+    <div className="w-screen h-[8vh] fixed top-0 p-4 md:py-4 md:px-8 flex flex-row justify-between items-center box-border bg-[#242424ba]">
+      <Link to="/">
+        <img src={logo} alt="VERAfied Technologies Logo" width="160px" />
+      </Link>
+      <ul className="flex flex-row gap-8 justify-center items-center">
+        <li>
+          <Link to="#services" className={linkStyle}>
+            Services
+          </Link>
+        </li>
+        <li>
+          <Link to="#work" className={linkStyle}>
+            Our Work
+          </Link>
+        </li>
+        <li>
+          <Link to="#contact" className={linkStyle}>
+            Contact Us
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,11 +1,15 @@
 import { Outlet, ScrollRestoration } from "react-router-dom";
+import { Navbar } from "../layouts/Navbar";
 
 // eslint-disable-next-line react/prop-types
 const RootLayout = () => {
   return (
     <>
+      <Navbar />
       <ScrollRestoration />
-      <Outlet />
+      <div>
+        <Outlet />
+      </div>
     </>
   );
 };
